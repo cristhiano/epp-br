52141003286776e11875fd6cdea8b9526dfb8615
Cristhiano Figueira <cristhiano_figueira@yahoo.com.br>
2015-07-12 14:49:21 -0300

    Credentials by config file

--- lib/epp-br/auth.rb
+++ lib/epp-br/auth.rb
@@ -2,19 +2,30 @@ module EPP
   module BR
     class Auth
       def initialize
-        ssl_context = OpenSSL::SSL::SSLContext.new(:TLSv1)
-        ssl_context.cert = OpenSSL::X509::Certificate.new(File.open('config/epp-br/client.pem'))
-        ssl_context.key = OpenSSL::PKey::RSA.new(File.open('config/epp-br/client.pem'))
-        ssl_context.ca_file = 'config/epp-br/root.pem'
+        require 'yaml'
+
+        credentials_file = YAML.load_file('config/epp-br/credentials.yml')
+        credentials = credentials_file['epp-br']['test']
+
+        ssl_cert  = OpenSSL::X509::Certificate.new(File.open(credentials['ssl']['cert']))
+        ssl_key   = OpenSSL::PKey::RSA.new(File.open(credentials['ssl']['key']))
+        ssl_ca    = credentials['ssl']['ca']
+
+        ssl_context =             OpenSSL::SSL::SSLContext.new(:TLSv1)
+        ssl_context.cert =        ssl_cert
+        ssl_context.key =         ssl_key
+        ssl_context.ca_file =     ssl_ca
         ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
         options = {ssl_context: ssl_context}
 
         @credentials = {
-          username: '405',
-          password: '123456',
-          host:     'beta.registro.br',
+          host:     credentials['host'],
+          username: credentials['username'].to_s,
+          password: credentials['password'].to_s,
           options:  options
         }
+
       end
 
       def username
